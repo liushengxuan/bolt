2dcb379b193a8e0333cf02738815e990ffe0148c
Shengxuan Liu <shengxuan.liu@bytedance.com>
2026-01-24 21:20:20 +0800

    Support Parquet Reader Decryption
    This commit introduces the decryption feature for Parquet reader. To use
    the decryption feature, you need to implement the KMS class for key
    retrieval.

---
+++ bolt/dwio/parquet/encryption/EncryptionType.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) ByteDance Ltd. and/or its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Partially inspired and adapted from Apache Arrow.
+
+#pragma once
+
+namespace bytedance::bolt::parquet {
+
+struct ParquetCipher {
+  enum type { AES_GCM_V1 = 0, AES_GCM_CTR_V1 = 1 };
+};
+
+} // namespace bytedance::bolt::parquet
